4fc0804c6764393bbbbd145386b811bb7ed9d919
Android Studio <devandroid@ort>
2022-04-05 10:36:21 -0300

    comenzada la integracion con el modal

---
+++ TP02/js/Todo.js
@@ -0,0 +1,10 @@
+class Todo {
+    constructor(name, desc) {
+        this.name = name;
+        this.desc = desc;
+        const timestamp = new Date().toLocaleString();
+        this.timestamp = timestamp;
+        this.completed = false;
+        this.id = null;
+    }
+}
--- TP02/js/index.js
+++ TP02/js/index.js
@@ -1,15 +1,3 @@
-/*class Todo {
-    constructor(name, desc) {
-        this.name = name;
-        this.desc = desc;
-        const timestamp = new Date().toLocaleString();
-        this.timestamp = timestamp;
-        this.completed = false;
-        this.id = null;
-    }
-}
-*/
-
 let todos = [];
 
 const div_todos = document.querySelector('#todos');
@@ -17,51 +5,56 @@ const nombre = document.querySelector('#task-name');
 const desc = document.querySelector('#task-description');
 const divTodoList = document.querySelector('#todo-list');
 const btn = document.querySelector('#save-task');
+const modal = document.querySelector('.modal');
 
-btn.addEventListener('click', (evnt) => {
-    // validar campos
-    let validado = ValidarCamposTodo();
-    if (validado == false) {
-        return false;
-    }
-    // crea un todo
-    let todo = new Todo(nombre.value, desc.value);
-    // agreagar el todo al array
-    todos.push(todo);
-    todo.id = todos.indexOf(todo);
-    // actualizar los todos del dom
-    MostrarTodo(todo);
-    // limpiar los inputs
-});
+
+function GuardarTodo() {
+  // evnt.preventDefault();
+  // validar campos
+  console.log(nombre)
+  let validado = ValidarCamposTodo();
+  if (validado == false) {
+    return false;
+  }
+  // crea un todo
+  let todo = new Todo(nombre.value, desc.value);
+  // agreagar el todo al array
+  todos.push(todo);
+  todo.id = todos.indexOf(todo);
+  // actualizar los todos del dom
+  MostrarTodo(todo);
+}
 
 
 function MostrarTodo(todo) {
-    let div = document.createElement('div');
-    let title = document.createElement('h3');
-    let desc = document.createElement('p');
-    let borrar = document.createElement('button');
-    let complete = document.createElement('button');
+  let div = document.createElement('div');
+  let title = document.createElement('h3');
+  let desc = document.createElement('p');
+  let borrar = document.createElement('button');
+  let checkbox = document.createElement('checkbox');
 
-    div.id = `todo-${todo.id}`;
-    title.id = `title-${todo.id}`;
-    desc.id = `desc-${todo.id}`;
-    borrar.id = `borrar-${todo.id}`;
-    complete.id = `complete-${todo.id}`;
+  div.id = `todo-${todo.id}`;
+  title.id = `title-${todo.id}`;
+  desc.id = `desc-${todo.id}`;
+  borrar.id = `borrar-${todo.id}`;
+  checkbox.id = `complete-${todo.id}`;
 
 
 
-    title.innerText = todo.name;
-    desc.innerText = todo.desc;
-    borrar.innerHTML = `<button onclick="BorrarTodo(${todo.id})">Borrar</button>`;
-    complete.innerHTML = `<button onclick="CompleteTodo(${todo.id})">Finalizar</button>`;
+  title.innerText = todo.name;
+  desc.innerText = todo.desc;
+  borrar.innerHTML = '';
+  checkbox.innerHTML = '';
+  borrar.innerHTML = `<button onclick="BorrarTodo(${todo.id})">Borrar</button>`;
+  checkbox.innerHTML = `<input type="checkbox" onclick="ToggleTodo(${todo.id})">`;
 
-    div.appendChild(title);
-    div.appendChild(desc);
-    div.appendChild(borrar);
-    div.appendChild(complete);
-
-    div_todos.appendChild(div);
+  div.appendChild(title);
+  div.appendChild(desc);
+  div.appendChild(borrar);
+  div.appendChild(checkbox);
+  div_todos.appendChild(div);
 }
+
 /*
 https://www.w3schools.com/howto/howto_js_todolist.asp
 
@@ -96,34 +89,39 @@ function newElement() {
 
 
 function BorrarTodo(id) {
-    const div_borrar = document.querySelector(`#todo-${id}`);
-    div_todos.removeChild(div_borrar);
-    todos.splice(id);
+  const div_borrar = document.querySelector(`#todo-${id}`);
+  div_todos.removeChild(div_borrar);
+  todos.splice(id);
 }
 
-function CompleteTodo(id) {
-    const div_completed = document.querySelector(`#todo-${id}`);
-    div_completed.style.color = 'red';
-
+function ToggleTodo(id) {
+  const div_completed = document.querySelector(`#todo-${id}`);
+  let e = EncontrarTodoXId(id);
+  console.log(e);
+  if (e.completed) {
+    div_completed.style.color = 'black';
+    todos[id].completed = false;
+  }
+  else{
+    div_completed.style.color = 'green';
     todos[id].completed = true;
-
+  }
 }
 
-
-
-//
-// const div_todos = document.querySelector('#todos');
-// let todosHtml = div_todos.childNodes;
-// todosHtml.forEach(todoHtml => {
-//     children = todoHtml.childNodes;
-//     console.log
-// });
-
-// //
 
 function ValidarCamposTodo() {
-    if (nombre.value == '') {
-        alert('falta nombre')
-        return false
+  if (nombre.value == '') {
+    alert('falta nombre')
+    return false
+  }
+}
+
+function EncontrarTodoXId(id) {
+  let element;
+  for (let i = 0; i < todos.length; i++) {
+    if (i == id) {
+      element = todos[i];
     }
+  }
+  return element
 }
